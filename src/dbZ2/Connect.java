4507c2160021e9d6695b7c4c54923b70bf08e64b
Old-Knitterhemd <fbnkrs@gmail.com>
2017-11-30 16:03:02 +0100

    zeugs

--- src/dbZ2/Connect.java
+++ src/dbZ2/Connect.java
@@ -1,77 +1,245 @@
 package dbZ2;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import oracle.jdbc.pool.OracleDataSource;
 
 public class Connect
 {
-	Connection dbConnection = null;
-	OracleDataSource ods = new OracleDataSource();
+	//DATENBANK VERBINDUNG
 
-	public Connect() throws SQLException, IOException
-	{
-		String treiber;
+	static Connection con = null;
 
-		treiber = "oracle.jdbc.driver.OracleDriver";
-
-		String uName;
-		String pW;
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Please enter username: ");
-		uName = in.readLine();
-		System.out.println("Please enter password: ");
-		pW = in.readLine();
-
-		// Treiber laden
-
-		try
-			{
-
-				Class.forName(treiber).newInstance();
-			} catch (Exception e)
-			{
-				System.out.println("Fehler beim laden des Treibers: " + e.getMessage());
-			}
-
-		// Erstellung Datenbank-Verbindungsinstanz
-		try
-			{
-
-				ods.setURL("jdbc:oracle:thin:" + uName + "/" + pW + "@schelling.nt.fh-koeln.de:1521:xe");
-				dbConnection = ods.getConnection();
-			} catch (SQLException e)
-			{
-				System.out.println("Fehler beim Verbindungsaufbau zur Datenbank!");
-				System.out.println(e.getMessage());
-			}
-		pW = "";
-		uName = "";
-
-	}
-
-	public Connection getDbConnection()
+	public void connect() throws IOException, SQLException
 		{
-			return dbConnection;
+			String treiber;
+			OracleDataSource ods = new OracleDataSource();
+
+			treiber = "oracle.jdbc.driver.OracleDriver";
+
+			String uName;
+			String pW;
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Please enter username: ");
+			uName = in.readLine();
+			System.out.println("Please enter password: ");
+			pW = in.readLine();
+
+			// Treiber laden
+
+			try
+				{
+
+					Class.forName(treiber).newInstance();
+				} catch (Exception e)
+				{
+					System.out.println("Fehler beim laden des Treibers: " + e.getMessage());
+				}
+
+			// Erstellung Datenbank-Verbindungsinstanz
+			try
+				{
+
+					ods.setURL("jdbc:oracle:thin:" + uName + "/" + pW + "@schelling.nt.fh-koeln.de:1521:xe");
+					con = ods.getConnection();
+				} catch (SQLException e)
+				{
+					System.out.println("Fehler beim Verbindungsaufbau zur Datenbank!");
+					System.out.println(e.getMessage());
+				}
+			pW = "";
+			uName = "";
 		}
 
-	public void setDbConnection(Connection dbConnection)
+	//SQL METHODEN
+	public int jdbcInsert() throws FileNotFoundException, IOException, SQLException
 		{
-			this.dbConnection = dbConnection;
+
+			FileReader fr1 = new FileReader("ARTIKEL.CSV");
+			BufferedReader br1 = new BufferedReader(fr1);
+
+			int inserts = 0;
+			String csvTemp;
+
+			String artnr, artbez, mge, prei, st;
+
+			while ((csvTemp = br1.readLine()) != null)
+				{
+					artnr = csvTemp.split(";")[0];
+					artbez = csvTemp.split(";")[1];
+					mge = csvTemp.split(";")[2];
+					prei = csvTemp.split(";")[3];
+					st = csvTemp.split(";")[4];
+					DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+
+					String date = df.format(Calendar.getInstance().getTime());
+
+					try
+						{
+							inserts += executeSQL("INSERT INTO ARTIKEL VALUES(" + artnr + ",'" + artbez + "'," + mge
+									+ ",'" + prei + "','" + st + "',TO_DATE ('" + date + "','DD-MM-YYYY'))");
+
+						} catch (SQLException a)
+						{
+							System.out.println("Exception geworfen!\n" + a.getMessage());
+							br1.close();
+							return -1;
+						}
+
+				}
+			br1.close();
+			return inserts;
 		}
 
-	public OracleDataSource getOds()
+	public int executeSQL(String sql) throws SQLException
 		{
-			return ods;
+			Statement Stmt;
+
+			int tempRS;
+			// Erzeugen eines Statements aus der DB-Verbindung
+			Stmt = con.createStatement();
+			// SQL-Anweisung ausführen und Ergebnis in ein ResultSet schreiben
+			tempRS = Stmt.executeUpdate(sql);
+			System.out.println(tempRS);
+			return tempRS;
 		}
 
-	public void setOds(OracleDataSource ods)
+	public ResultSet executeSQLquery(String sql, String tab) throws SQLException
 		{
-			this.ods = ods;
+			Statement Stmt;
+
+			ResultSet tempRS;
+
+			// Erzeugen eines Statements aus der DB-Verbindung
+			Stmt = con.createStatement();
+
+			// SQL-Anweisung ausführen und Ergebnis in ein ResultSet schreiben
+			tempRS = Stmt.executeQuery(sql + tab);
+			return tempRS;
 		}
 
+	public void rsMetaAusgabe(ResultSet RS) throws SQLException
+		{
+			ResultSetMetaData rsmd = RS.getMetaData();
+			System.out.println("\n\n");
+			String columnNames = "";
+			String data = "";
+
+			for (int i = 1; i <= rsmd.getColumnCount(); i++)
+				{
+					columnNames += rsmd.getColumnName(i) + "\t\t\t";
+				}
+			System.out.println(columnNames);
+			while (RS.next())
+				{
+					data = "";
+					for (int i = 1; i <= rsmd.getColumnCount(); i++)
+						{
+							if (RS.getString(i).length() >= 8)
+								{
+									if (RS.getString(i).length() >= 16)
+										{
+											data += RS.getString(i) + "\t";
+										} else
+										{
+											data += RS.getString(i) + "\t\t";
+										}
+								} else
+								{
+									data += RS.getString(i) + "\t\t\t";
+								}
+						}
+					System.out.println(data);
+				}
+			RS.close();
+		}
+
+	public int sqlHandler(int c, String csv) throws SQLException
+		{
+			switch (c)
+				{
+
+				case 1:
+					String sALL = "SELECT * FROM ";
+					if (csv.equals("ARTIKEL"))
+						{
+							rsMetaAusgabe(executeSQLquery(
+									"SELECT ARTIKEL.ARTNR, ARTIKEL.ARTBEZ, ARTIKEL.MGE , ARTIKEL.PREIS, ARTIKEL.STEU, TO_CHAR(ARTIKEL.EDAT,'DD-MM-YYYY') EDAT FROM ",
+									"ARTIKEL"));
+							return 1;
+						} else if (csv.equals("LAGER"))
+						{
+
+							rsMetaAusgabe(executeSQLquery(sALL, "LAGER"));
+							return 1;
+						} else if (csv.equals("KUNDE"))
+						{
+
+							rsMetaAusgabe(executeSQLquery(sALL, "KUNDE"));
+							return 1;
+						}
+					break;
+
+				case 2://suche nach artnr
+					try
+						{
+
+							int t = Integer.parseInt(csv);
+							csv = String.valueOf(t);
+						} catch (Exception e)
+						{
+
+							System.out.println("Keine gültige Artikel Nummer\n" + e.getMessage());
+							return -1;
+
+						}
+
+					rsMetaAusgabe(executeSQLquery(
+							"SELECT ARTIKEL.ARTNR, ARTIKEL.ARTBEZ, ARTIKEL.MGE, ARTIKEL.PREIS, ARTIKEL.STEU, TO_CHAR(ARTIKEL.EDAT,'DD-MM-YYYY') EDAT , LAGERBESTAND.BSTNR, LAGERBESTAND.LNR, LAGERBESTAND.MENGE, LAGER.LORT, LAGER.LPLZ FROM ARTIKEL,LAGERBESTAND,LAGER ",
+							("WHERE ARTIKEL.ARTNR=" + csv + " AND LAGERBESTAND.ARTNR=" + csv
+									+ " AND LAGER.LNR= LAGERBESTAND.LNR")));
+
+					rsMetaAusgabe(sumi(csv));
+					return 1;
+
+				case 3: //neuer lagerbestand
+
+					String[] csvA = csv.split(";");
+
+					return executeSQL("INSERT INTO LAGERBESTAND VALUES (null, " + csvA[0] + ", " + csvA[1] + ", "
+							+ csvA[2] + ")");
+
+				case 4: // update bestand
+					String[] csvA2 = csv.split(";");
+
+					return executeSQL("UPDATE LAGERBESTAND SET LAGERBESTAND.MENGE = " + csvA2[2]
+							+ " WHERE LAGERBESTAND.ARTNR= " + csvA2[0] + " AND LAGERBESTAND.LNR= " + csvA2[1]);
+
+				}
+			return -2;
+		}
+
+	private ResultSet sumi(String csv) throws SQLException
+		{
+
+			return executeSQLquery("SELECT SUM (LAGERBESTAND.MENGE) Gesamtbestand  FROM LAGERBESTAND",
+					" WHERE LAGERBESTAND.ARTNR = " + csv);
+		}
+
+	public void jdbcBestellung(String knr, String artnr, String bmenge)
+		{
+			
+		}
 }
--- src/dbZ2/JDBC_Verwaltung.java
+++ src/dbZ2/JDBC_Verwaltung.java
@@ -1,166 +1,12 @@
 package dbZ2;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import oracle.jdbc.pool.OracleDataSource;
 
 public class JDBC_Verwaltung
 {
-
-	//DATENBANK VERBINDUNG
-
-	static Connection con = null;
-
-	public static Connection connect() throws IOException, SQLException
-		{
-			String treiber;
-			OracleDataSource ods = new OracleDataSource();
-
-			treiber = "oracle.jdbc.driver.OracleDriver";
-			Connection dbConnection = null;
-			String uName;
-			String pW;
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("Please enter username: ");
-			uName = in.readLine();
-			System.out.println("Please enter password: ");
-			pW = in.readLine();
-
-			// Treiber laden
-
-			try
-				{
-
-					Class.forName(treiber).newInstance();
-				} catch (Exception e)
-				{
-					System.out.println("Fehler beim laden des Treibers: " + e.getMessage());
-				}
-
-			// Erstellung Datenbank-Verbindungsinstanz
-			try
-				{
-
-					ods.setURL("jdbc:oracle:thin:" + uName + "/" + pW + "@schelling.nt.fh-koeln.de:1521:xe");
-					dbConnection = ods.getConnection();
-				} catch (SQLException e)
-				{
-					System.out.println("Fehler beim Verbindungsaufbau zur Datenbank!");
-					System.out.println(e.getMessage());
-				}
-			pW = "";
-			uName = "";
-			return dbConnection;
-		}
-
-	//SQL METHODEN
-	public static int jdbcInsert() throws FileNotFoundException, IOException, SQLException
-		{
-
-			FileReader fr1 = new FileReader("ARTIKEL.CSV");
-			BufferedReader br1 = new BufferedReader(fr1);
-
-			int inserts = 0;
-			String csvTemp;
-
-			String artnr, artbez, mge, prei, st;
-
-			while ((csvTemp = br1.readLine()) != null)
-				{
-					artnr = csvTemp.split(";")[0];
-					artbez = csvTemp.split(";")[1];
-					mge = csvTemp.split(";")[2];
-					prei = csvTemp.split(";")[3];
-					st = csvTemp.split(";")[4];
-					DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-
-					String date = df.format(Calendar.getInstance().getTime());
-
-					try
-						{
-							inserts += executeSQL("INSERT INTO ARTIKEL VALUES(" + artnr + ",'" + artbez + "'," + mge
-									+ ",'" + prei + "','" + st + "',TO_DATE ('" + date + "','DD-MM-YYYY'))");
-
-						} catch (SQLException a)
-						{
-							System.out.println("Exception geworfen!\n" + a.getMessage());
-							br1.close();
-							return -1;
-						}
-
-				}
-			br1.close();
-			return inserts;
-		}
-
-	public static int executeSQL(String sql) throws SQLException
-		{
-			Statement Stmt;
-
-			int tempRS;
-			// Erzeugen eines Statements aus der DB-Verbindung
-			Stmt = con.createStatement();
-			// SQL-Anweisung ausführen und Ergebnis in ein ResultSet schreiben
-			tempRS = Stmt.executeUpdate(sql);
-			return tempRS;
-		}
-
-	public static ResultSet executeSQLquery(String sql, String tab) throws SQLException
-		{
-			Statement Stmt;
-
-			ResultSet tempRS;
-
-			// Erzeugen eines Statements aus der DB-Verbindung
-			Stmt = con.createStatement();
-
-			// SQL-Anweisung ausführen und Ergebnis in ein ResultSet schreiben
-			tempRS = Stmt.executeQuery(sql + tab);
-			return tempRS;
-		}
-
-	public static void rsMetaHandling(ResultSet RS) throws SQLException
-		{
-			ResultSetMetaData rsmd = RS.getMetaData();
-			System.out.println("\n\n");
-			String columnNames = "";
-			String data = "";
-
-			for (int i = 1; i <= rsmd.getColumnCount(); i++)
-				{
-					columnNames += rsmd.getColumnName(i) + "\t\t\t";
-				}
-			System.out.println(columnNames);
-			while (RS.next())
-				{
-					data = "";
-					for (int i = 1; i <= rsmd.getColumnCount(); i++)
-						{
-							if (RS.getString(i).length() >= 8)
-								{
-									data += RS.getString(i) + "\t\t";
-
-								} else
-								{
-									data += RS.getString(i) + "\t\t\t";
-								}
-						}
-					System.out.println(data);
-				}
-			RS.close();
-		}
 
 	//MAIN VERWALTUNG UND MENÜ
 	public static void main(String[] args) throws IOException
@@ -171,22 +17,24 @@ public class JDBC_Verwaltung
 			// Datenbankverbindung erstellen mit Hilfe der "connect()"-Methode
 			try
 				{
-					con = connect();
+					// Verbindungsobjekt wird erstellt
+					Connect con = new Connect();
+					// Verbindung wird aufgebaut
+					con.connect();
 
-					// JDBC Objekte
-					//	Statement Stmt = con.createStatement();
-					ResultSet RS;
+				
 
 					//MENUE
 					int choice = 0;
 					do
 						{
 							System.out.println("Willkommen im Menue:");
-							System.out.println("(1)Neue Datensätze importieren mit JDBC INSERT");
-							System.out.println("(2)Datensätze abrufen");
-							System.out.println("(3)Artikel Auskunft, nach ArtikelNr");
-							System.out.println("(4)Neuer Lagerbestand für Artikel");
-							System.out.println("(5)Menge eines Lagerbestandes aktualisieren");
+							
+							System.out.println("(1)Datensätze abrufen");
+							System.out.println("(2)Artikel Auskunft, nach ArtikelNr");
+							System.out.println("(3)Neuer Lagerbestand für Artikel");
+							System.out.println("(4)Menge eines Lagerbestandes aktualisieren");
+							System.out.println("(5)Neue Datensätze importieren mit JDBC INSERT");
 							System.out.println("(0)Programm beenden");
 
 							try
@@ -201,20 +49,7 @@ public class JDBC_Verwaltung
 							switch (choice)
 								{
 								case 1:
-									System.out.println("Datensätze werden aus ARTIKEL.CSV importiert...\n\n");
-									int a = jdbcInsert();
-									if (a >= 1)
-										{
-											System.out.println("Update erfolgreich!\nEingepflegte Datensätze: " + a);
-										} else
-										{
-											System.out.println("Update fehlgeschlagen!");
-										}
-
-									System.out.println("\n\n");
-									break;
-
-								case 2:
+																
 									//SUB MENÜ
 
 									do
@@ -234,30 +69,30 @@ public class JDBC_Verwaltung
 													System.out.println(e.getMessage());
 												}
 
-											String sALL = "SELECT * FROM ";
 											switch (choice)
 
 												{
 												case 1:
 													System.out.println("\n\n");
-													RS = executeSQLquery(
-															"SELECT ARTIKEL.ARTNR, ARTIKEL.ARTBEZ, ARTIKEL.MGE , ARTIKEL.PREIS, ARTIKEL.STEU, TO_CHAR(ARTIKEL.EDAT,'DD-MM-YYYY') EDAT FROM ",
-															"ARTIKEL");
-													rsMetaHandling(RS);
+
+													con.sqlHandler(1, "ARTIKEL");
+
 													System.out.println("\n\n");
 													break;
 
 												case 2:
 													System.out.println("\n\n");
-													RS = executeSQLquery(sALL, "LAGER");
-													rsMetaHandling(RS);
+
+													con.sqlHandler(1, "LAGER");
+
 													System.out.println("\n\n");
 													break;
 
 												case 3:
 													System.out.println("\n\n");
-													RS = executeSQLquery(sALL, "KUNDE");
-													rsMetaHandling(RS);
+
+													con.sqlHandler(1, "KUNDE");
+
 													System.out.println("\n\n");
 													break;
 												}//SWITCH END
@@ -268,24 +103,17 @@ public class JDBC_Verwaltung
 									break;
 
 								//ARTIKEL SUCHE NACH ARTNR
-								case 3:
+								case 2:
 									System.out.println("\n\n");
 									System.out.println("Bitte Artikel Nr eingeben: ");
 									String artNr = in.readLine();
-									RS = executeSQLquery(
-											"SELECT ARTIKEL.ARTNR, ARTIKEL.ARTBEZ, ARTIKEL.MGE, ARTIKEL.PREIS, ARTIKEL.STEU, TO_CHAR(ARTIKEL.EDAT,'DD-MM-YYYY') EDAT , LAGERBESTAND.BSTNR, LAGERBESTAND.LNR, LAGERBESTAND.MENGE, LAGER.LORT, LAGER.LPLZ FROM ARTIKEL,LAGERBESTAND,LAGER ",
-											("WHERE ARTIKEL.ARTNR=" + artNr + " AND LAGERBESTAND.ARTNR=" + artNr
-													+ " AND LAGER.LNR= LAGERBESTAND.LNR"));
-									rsMetaHandling(RS);
 
-									RS = executeSQLquery(
-											"SELECT SUM (LAGERBESTAND.MENGE) Gesamtbestand  FROM LAGERBESTAND",
-											" WHERE LAGERBESTAND.ARTNR = " + artNr);
-									rsMetaHandling(RS);
+									con.sqlHandler(2, artNr);
+
 									System.out.println("\n\n");
 									break;
 
-								case 4: //neuer lagerbestand
+								case 3: //neuer lagerbestand
 									System.out.println("\n\n");
 									// User Abfragen
 									System.out.println(
@@ -298,15 +126,17 @@ public class JDBC_Verwaltung
 											"Wie lautet der neue Bestand?\nBitte Bestand in ganzen zahlen eingeben: ");
 									String menge = in.readLine();
 
+									String csv = (artNrTemp + ";" + laNr + ";" + menge);
 									//SQL GESCHWURBEL
+									if (con.sqlHandler(3, csv)==1)
+										{
+											System.out.println("Erfolgreich angelegt!");
+										}
 
-									System.out.println("Angefügte Bestände: "
-											+ executeSQL("INSERT INTO LAGERBESTAND VALUES (null, " + artNrTemp + ", "
-													+ laNr + ", " + menge + ")"));
 									System.out.println("\n\n");
 									break;
 
-								case 5: //update
+								case 4: //UPDATE
 									int updates = 0;
 
 									// User Abfragen
@@ -319,17 +149,31 @@ public class JDBC_Verwaltung
 									System.out.println(
 											"Wie lautet der neue Bestand?\nBitte Bestand in ganzen zahlen eingeben: ");
 									String menge2 = in.readLine();
-
+									String csv2 = (artNrTemp2 + ";" + laNr2 + ";" + menge2);
 									//SQL geschwurbel
 									System.out.println("\n\n");
-									updates = executeSQL("UPDATE LAGERBESTAND SET LAGERBESTAND.MENGE = " + menge2
-											+ " WHERE LAGERBESTAND.ARTNR= " + artNrTemp2 + " AND LAGERBESTAND.LNR= "
-											+ laNr2);
+																		
+									updates = con.sqlHandler(4, csv2);
 									System.out.println("Geänderte Bestände: " + updates);
 
 									System.out.println("\n\n");
 									break;
+									
+									
+								case 5: //JDBC INSERT
+								
+									System.out.println("Datensätze werden aus ARTIKEL.CSV importiert...\n\n");
+									int a = con.jdbcInsert();
+									if (a >= 1)
+										{
+											System.out.println("Update erfolgreich!\nEingepflegte Datensätze: " + a);
+										} else
+										{
+											System.out.println("Update fehlgeschlagen!");
+										}
 
+									System.out.println("\n\n");
+									break;
 								}
 						} while (choice != 0);
 					System.out.println("...shutting down..\n\nBYE BYE!!!");
